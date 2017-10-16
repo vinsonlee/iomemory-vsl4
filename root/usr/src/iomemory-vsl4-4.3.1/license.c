400479dd0ea6e19d5cfa9ba3683e942debbf3b8d
Vinson Lee <vlee@freedesktop.org>
2017-10-16 20:55:43 +0000

    Add iomemory-vsl4_4.3.1.836 source.
    Signed-off-by: Vinson Lee <vlee@freedesktop.org>

---
+++ root/usr/src/iomemory-vsl4-4.3.1/license.c
@@ -0,0 +1,7 @@
+/* Machine generated file - edits will be overwritten. */
+
+#include <linux/module.h>
+MODULE_LICENSE("Proprietary");
+MODULE_AUTHOR("SanDisk Corp.");
+MODULE_DESCRIPTION("Fusion ioMemory-VSL (Virtual Storage Layer)");
+MODULE_INFO(srcversion, "4.3.1.836 bryce@463420716dc9");
