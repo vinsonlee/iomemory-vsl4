e71c42340b7babfa51b231ab743d47379b36f595
Vinson Lee <vlee@freedesktop.org>
2020-04-05 16:52:46 -0700

    Add iomemory-vsl4-4.3.7.1205 source.
    Signed-off-by: Vinson Lee <vlee@freedesktop.org>

---
+++ root/usr/src/iomemory-vsl4-4.3.7/license.c
@@ -0,0 +1,7 @@
+/* Machine generated file - edits will be overwritten. */
+
+#include <linux/module.h>
+MODULE_LICENSE("Proprietary");
+MODULE_AUTHOR("Western Digital Technologies, Inc.");
+MODULE_DESCRIPTION("Fusion ioMemory-VSL (Virtual Storage Layer)");
+MODULE_INFO(srcversion, "4.3.7.1205 bryce@a7e621e6fb50");
